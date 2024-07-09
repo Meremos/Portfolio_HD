54f26a4b6491358a04dd7205df294d5fe8299cb6
Meremos <info@davidherve.ch>
2024-07-09 13:49:37 +0200

    add image linked in

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -13,7 +13,8 @@ export default function Footer() {
         </div>
       <div className="footer-item">
       <a href="https://www.linkedin.com/in/davidh4/" target="_blank" rel="noopener noreferrer">
-          <img src={`${process.env.PUBLIC_URL}/img/icon_linkedin.svg`} alt="LinkedIn Icon" />
+          <img src="/Portfolio_HD/img/icon_linkedin.svg" alt="LinkedIn Icon" />
+          {/* <img src={`${process.env.PUBLIC_URL}/img/icon_linkedin.svg`} alt="LinkedIn Icon" /> */}
         </a>
         </div>
       <div className="footer-item">
