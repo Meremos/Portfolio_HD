53e062b95923e7c30cef88a7ba5d4aec8359f481
Meremos <info@davidherve.ch>
2024-07-10 12:20:29 +0200

    modif home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 // App.js
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 
-import Home from './pages/home'; // Use the correct filename with a small "h"
-import Contact from './pages/contact';
-import Portfolio from './pages/portfolio';
+import Home from './pages/home'; // Ensure the correct path
 import AboutMe from './pages/about_me';
-import UseCases from './pages/use_cases';
 
 import './Styles_css/App.css';
 import './Styles_css/Nav.css';
@@ -18,11 +15,9 @@ function App() {
     <div>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/about_me" element={<AboutMe />} />
-        <Route path="/use_cases" element={<UseCases />} />
       </Routes>
     </div>
   );
